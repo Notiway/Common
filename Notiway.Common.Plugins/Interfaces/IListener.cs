d9d8f64d724d261bd23024b5022fef6ab8533a72
Puc <jakub.paulina.sk@gmail.com>
2026-01-12 18:01:47 +0100

    Update IListener.ConsumeAsync to require queueTarget param
    Modified the IListener interface so that ConsumeAsync now requires a queueTarget parameter to identify the queue (e.g., name or URL). Added XML documentation to clarify method usage and parameters.

--- Notiway.Common.Plugins/Interfaces/IListener.cs
+++ Notiway.Common.Plugins/Interfaces/IListener.cs
@@ -1,5 +1,15 @@
 ﻿namespace Notiway.Common.Plugins.Interfaces;
 public interface IListener
 {
-    Task ConsumeAsync<TMessage>(Func<TMessage, Task> ProcessMessageAsync, CancellationToken cancellationToken, int retry = 3);
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <param name="queueTarget">Can be queueName or queueUrl or anything that identifies queue. 
+    /// Target should be result of queue creation from IQueueManager</param>
+    /// <param name="ProcessMessageAsync"></param>
+    /// <param name="cancellationToken"></param>
+    /// <param name="retry"></param>
+    /// <returns></returns>
+    Task ConsumeAsync<TMessage>(string queueTarget, Func<TMessage, Task> ProcessMessageAsync, CancellationToken cancellationToken, int retry = 3);
 }
