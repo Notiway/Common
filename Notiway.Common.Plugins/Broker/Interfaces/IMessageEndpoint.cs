5aed1d72cfce8decde87e279d6cbb7738a9f7a88
Puc <jakub.paulina.sk@gmail.com>
2026-01-15 18:00:41 +0100

    Add Name and Id properties to IMessageEndpoint interface
    Added Name and Id string properties to the IMessageEndpoint interface to provide additional identification for message endpoints, supplementing the existing Address, Type, and Metadata properties.

--- Notiway.Common.Plugins/Broker/Interfaces/IMessageEndpoint.cs
+++ Notiway.Common.Plugins/Broker/Interfaces/IMessageEndpoint.cs
@@ -5,6 +5,10 @@ namespace Notiway.Common.Plugins.Broker.Interfaces;
 public interface IMessageEndpoint
 {
     string Address { get; }
+
+    string Name { get; }
+
+    string Id { get; }
     EndpointType Type { get; }
     IReadOnlyDictionary<string, string>? Metadata { get; }
 }
